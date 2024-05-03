3b601fed718f6ad74474fa1533c98919a2b32713
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-05-03 08:09:23 +0200

    fix: improve error messages from API errors

--- src/rejectOnApiError.js
+++ src/rejectOnApiError.js
@@ -3,7 +3,13 @@ const miss = require('mississippi')
 module.exports = () =>
   miss.through.obj((doc, enc, callback) => {
     if (doc.error && doc.statusCode) {
-      callback(new Error([doc.statusCode, doc.error].join(': ')))
+      callback(
+        new Error(
+          ['Export', `HTTP ${doc.statusCode}`, doc.error, doc.message]
+            .filter((part) => typeof part === 'string')
+            .join(': '),
+        ),
+      )
       return
     }
 
