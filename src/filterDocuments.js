869bfb2f4e3124395c221e237a1c7f3c33d0a43d
Robin Neatherway <rneatherway@sanity.io>
2025-05-09 18:10:20 +0100

    feat: update export for releases

---
+++ src/filterDocuments.js
@@ -0,0 +1,34 @@
+const miss = require('mississippi')
+const debug = require('./debug')
+
+const isDraftOrVersion = (doc) => doc && doc._id && (
+  doc._id.indexOf('drafts.') === 0 ||
+  doc._id.indexOf('versions.') === 0
+)
+
+const isSystemDocument = (doc) => doc && doc._id && doc._id.indexOf('_.') === 0
+const isRelease = (doc) => doc && doc._id && doc._id.indexOf('_.releases.') === 0
+const isCursor = (doc) => doc && !doc._id && doc.nextCursor !== undefined
+
+module.exports = (drafts) =>
+  miss.through.obj((doc, enc, callback) => {
+    if (isCursor(doc)) {
+      debug('%o is a cursor, skipping', doc)
+      return callback()
+    }
+
+    if (!drafts && isDraftOrVersion(doc)) {
+      debug('%s is a draft or version, skipping', doc && doc._id)
+      return callback()
+    }
+
+    if (isSystemDocument(doc)) {
+      if (!drafts && isRelease(doc)) {
+        return callback(null, doc)
+      }
+      debug('%s is a system document, skipping', doc && doc._id)
+      return callback()
+    }
+
+    return callback(null, doc)
+  }) 
